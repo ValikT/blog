a1143f8daa4c613b9da481befc93bd09142f237a
ValikT <valentint711@gmail.com>
2017-12-01 20:33:34 +0200

    Likes already work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,58 +1,55 @@
 class PhotosController < ApplicationController
 before_action :authenticate_user!
+
 def index
 @photos = Photo.all
 @like = {}
 @photos.each do |photo|
-	@votes = Vote.where(photo_id: photo.id).sum(:like)
-		@like[photo.id] = @votes
-		end
-	end
+@votes = Vote.where(photo_id: photo.id).sum(:like)
+@like[photo.id] = @votes
+end
+
 end
 
 def new
 @photo = Photo.new
 end
 
-  def create
-      params[:photo][:category_id] = params[:category_id]
-      params[:photo][:user_id] = current_user.id
-   	@photo = Photo.create(photo_params)
-      redirect_to photos_path
-  end
-
-
-def edit
+def create
+params[:photo][:category_id] = params[:category_id]
+params[:photo][:user_id] = current_user.id
+params[:photo][:photo]
+@photo = Photo.create(photo_params)
+redirect_to photos_path
 end
 
-def destoy
+def edit
 end
 
 def update
 end
 
-def create
-params[:photo][:category_id] = params[:category_id]
-params[:photo][:user_id] = current_user.id
-@photo = Photo.create(photo_params)
+def destroy
+@photo.destroy
 redirect_to photos_path
 end
 
 def vote
-	pp @photo = Photo.find(params[:id])
-	pp @vote = Vote.where(user_id: current_user.id, photo_id: @photo.id).first
-	if @vote == nil or @vote.like == 0
-		@like = 1
-		Vote.create(user_id: current_user.id,photo_id: @photo.id,like: @like)
-	else 
-		@vote.update(like: @vote.like+1)
-	end
-
-	redirect_to photos_path
+@photo = Photo.find(params[:id])
+@vote = Vote.where(user_id: current_user.id, photo_id: @photo.id).first
+if @vote == nil or @vote.like == 0
+@like = 1
+Vote.create(user_id: current_user.id, photo_id: @photo.id, like: @like)
+else
+@vote.update(like: @vote.like+1)
+end
+redirect_to photos_path
 end
 
 private
 
 def photo_params
 params.require(:photo).permit(:user_id, :category_id, :name, :photo)
+end
+
 end
